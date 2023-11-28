34db35b4ac6c7774d155ba003ceae39aaa8a6da2
Sevindu Geshan <sevindu5662@gmail.com>
2023-11-28 18:33:17 +0530

    files

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, request, redirect, url_for
+from plot_pred import pred_and_plot_image
+from model.modelClass import ResNet18
+from torchvision import transforms
+import torch
+from pathlib import Path
+from timeit import default_timer as timer
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('System_Fundamental.html', result=None)
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    model_path = Path("model/Model2.pth")
+
+    # Define the class names for predictions
+    class_names = ['Actinic keratosis',
+                   'Basal cell carcinoma',
+                   'Benign keratosis',
+                   'Dermatofibroma',
+                   'Melanocytic nevus',
+                   'Melanoma',
+                   'Squamous cell carcinoma',
+                   'Vascular lesion']
+
+    # Load your deep learning model
+    model = ResNet18()  # Replace with the actual class of your model
+
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    manual_transforms = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize])
+
+    # Get the uploaded image file
+    images = request.files.getlist("image")
+
+    labels = []
+    probs = []
+    image_paths = []
+
+    start_time = timer()
+
+    for image in images:
+        img_path = "static/" + image.filename
+        image.save(img_path)
+        try:
+            final = pred_and_plot_image(class_names=class_names,
+                                        model=model,
+                                        image_path=img_path,
+                                        transform=manual_transforms)
+            labels.append(final[0])
+            probs.append(final[1])
+        except Exception as e:
+            print("Exception: ",str(e))
+        image_paths.append(img_path)
+
+    end_time = timer()
+    total_time = end_time - start_time
+    data = {"image_name": image_paths,
+            "Labels": labels,
+            "Probs": probs}
+    tput = len(images) / (total_time)
+    latency = 1 / tput
+    latency = round(latency, 4)
+    tput = round(tput, 4)
+    total_time = round(total_time, 4)
+    return render_template('testinghtml.html',
+                           data=data,
+                           lenght=total_time,
+                           tput=tput,
+                           latency=latency)
+
+
+@app.route('/goback')
+def go_back():
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ plot_pred.py
@@ -0,0 +1,57 @@
+import torchvision
+from torchvision import transforms
+from typing import List, Tuple
+from PIL import Image
+import torch
+
+
+
+
+def pred_and_plot_image(
+    model: torch.nn.Module,
+    class_names: List[str],
+    image_path: str,
+    image_size: Tuple[int, int] = (224, 224),
+    transform: torchvision.transforms = None
+):
+
+
+    # Open image
+    img = Image.open(image_path)
+
+    # Create transformation for image (if one doesn't exist)
+    if transform is not None:
+        image_transform = transform
+    else:
+        image_transform = transforms.Compose(
+            [
+                transforms.Resize(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+
+    ### Predict on image ###
+
+    # Turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+        transformed_image = image_transform(img).unsqueeze(dim=0)
+
+        # Make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(transformed_image)
+
+    # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+    pred_label = class_names[target_image_pred_label]
+    pred_probabil = target_image_pred_probs.max().detach().numpy()
+
+    results = [pred_label, pred_probabil]
+    return results
